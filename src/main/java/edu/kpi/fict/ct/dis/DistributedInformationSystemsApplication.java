e970fd362389a9074ed714f70e4b9fdb4e3d9414
Yurii <yuriy.nesteruk@yandex.ru>
2016-06-23 01:37:18 +0300

    'main' class added

---
+++ src/main/java/edu/kpi/fict/ct/dis/DistributedInformationSystemsApplication.java
@@ -0,0 +1,12 @@
+package edu.kpi.fict.ct.dis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedInformationSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedInformationSystemsApplication.class, args);
+	}
+}
